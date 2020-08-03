3fed1986a7b55470bc6012233301aecbc5cc5fc4
Sergei Zakharov <sergei86zahar@mail.ru>
2020-08-03 22:26:05 +0700

    2

--- src/Учить_31/_Глава_3_4_5/Stack.java
+++ src/Учить_31/_Глава_3_4_5/Stack.java
@@ -3,11 +3,11 @@ public class Stack {
     int[] stck = new int[10];
     int tos;
     //инициализировать вершину стека
-    Stack() {
+    Stack () {
         tos = -1;
     }
     //поместить элемент в стек
-    void push(int item) {
+    void push (int item) {
         if (tos == 9) {
             System.out.println("Стек заполнен.");
         } else {
@@ -15,7 +15,7 @@ public class Stack {
         }
     }
     //извлечь элемент из стека
-    int pop() {
+    int pop () {
         if (tos < 0) {
             System.out.println("стек пуст");
             return 0;
@@ -25,22 +25,74 @@ public class Stack {
     }
 }
 class Stack33 {
-   public static void main(String[] args) {
-      Stack mystack1 = new Stack();
-      Stack mystack2 = new Stack();
-      //поместить несколько чисел в стек
-      for (int i = 0; i < 10; i++) mystack1.push(i);
-      for (int i = 10; i < 20; i++) mystack2.push(i);
-      //вытолкнуть эти числа из стека
-      System.out.println("Стек в mystack1: ");
-      for (int i = 0; i < 10; i++) {
-         System.out.println(mystack1.pop());
-      }
-      System.out.println("Стек в mystack2: ");
-      for (int i = 0; i < 10; i++) {
-         System.out.println(mystack2.pop());
-      }
-   }
+    public static void main (String[] args) {
+        Stack mystack1 = new Stack();
+        Stack mystack2 = new Stack();
+        //поместить несколько чисел в стек
+        for (int i = 0; i < 10; i++) {
+            mystack1.push(i);
+        }
+        for (int i = 10; i < 20; i++) {
+            mystack2.push(i);
+        }
+        //вытолкнуть эти числа из стека
+        System.out.println("Стек в mystack1: ");
+        for (int i = 0; i < 10; i++) {
+            System.out.print(mystack1.pop());
+        }
+        System.out.println();
+        System.out.println("Стек в mystack2: ");
+        for (int i = 0; i < 10; i++) {
+            System.out.print(mystack2.pop());
+        }
+        System.out.println();
+        System.out.println("Моя реализация");
+        System.out.println();
+        StackClon33 stackClon33 = new StackClon33();
+        stackClon33.main1();
+    }
 }
+class StackClon {
+    int[] stck = new int[10];
+    int tos;
+    StackClon () {
+        tos = -1;
+    }
+    void push (int item) {
+        if (tos == 9) {
+            System.out.println("Стек полон");
+        } else {
+            stck[++tos] = item;
+        }
+    }
+    int pop () {
+        if (tos < 0) {
+            System.out.println("Стек пустой");
+            return 0;
+        } else {
+            return stck[tos--];
+        }
+    }
+}
+class StackClon33 {
+    void main1(){
+        StackClon stackClon1 = new StackClon();
+        StackClon stackClon2 = new StackClon();
+        for (int i = 0; i<10;i++){
+            stackClon1.push(i);
+        }
+        for (int i = 0; i<10; i++){
+            stackClon2.push(i);
+        }
+        System.out.println("Содержимое stackClon1");
+        for (int i = 0; i<10;i++){
+            System.out.print(stackClon1.pop());
 
+        }
+        System.out.println("Содержимое stackClon2");
+        for (int i =0; i<10; i++){
+            System.out.print(stackClon2.pop());
+        }
+    }
+}
 
--- src/Учить_31/_Глава_3_4_5/_Многомерный_массив2.java
+++ src/Учить_31/_Глава_3_4_5/_Многомерный_массив2.java
@@ -1,8 +1,8 @@
 package Учить_31._Глава_3_4_5;
 public class _Многомерный_массив2 {
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         int[][] w = new int[4][];
-        w[0]= new int[3];
+        w[0] = new int[3];
         w[1] = new int[8];
         w[2] = new int[9];
         w[3] = new int[6];
@@ -16,6 +16,31 @@ public class _Многомерный_массив2 {
         for (i = 0; i < w.length; i++) {
             for (e = 0; e < w[i].length; e++) {
                 System.out.print(w[i][e] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println();
+        Clon clon = new Clon();
+        clon.main1();
+    }
+}
+class Clon {
+    void main1 () {
+        int[][] q = new int[3][];
+        q[0] = new int[4];
+        q[1] = new int[4];
+        q[2] = new int[4];
+        int r, t, y = 0;
+        for (r = 0; r < q.length; r++) {
+            for (t = 0; t < q[r].length; t++) {
+                q[r][t] = y;
+                y++;
+            }
+        }
+        for (r = 0; r<q.length;r++){
+            for (t = 0; t < q[r].length; t++){
+                System.out.print(q[r][t] + " ");
             }
             System.out.println();
         }
