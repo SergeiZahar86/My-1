321cc398a57c563508298c249c2ee9238890e41c
serge <sergeizahargood@gmail.com>
2020-09-11 08:52:49 +0700

    File

---
+++ src/_Java8/_Глава_21_IO/_797_File/File.java
@@ -0,0 +1,9 @@
+package _Java8._Глава_21_IO._797_File;
+public class File {
+    private void print (String s) {
+        System.out.println(s);
+    }
+    public static void main (String[] args) {
+
+    }
+}
