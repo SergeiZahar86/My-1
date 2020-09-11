3f1aa69da9c424bca7f9a276fcbae2e705d7c85d
serge <sergeizahargood@gmail.com>
2020-09-11 09:11:04 +0700

    File

--- src/_Java8/_Глава_21_IO/_797_File/File1.java
+++ src/_Java8/_Глава_21_IO/_797_File/File1.java
@@ -1,10 +1,12 @@
 package _Java8._Глава_21_IO._797_File;
 import java.io.File;
 public class File1 {
-    private void print (String s) {
+    private static void print (String s) {
         System.out.println(s);
     }
     public static void main (String[] args) {
-        File file = new File("C://Program Files (x86)/QTranslate.exe");
+        File file = new File("C://Empty folder/11.txt");
+        print("File Name: " + file.getName());
+
     }
 }
