891cb1fcf3997bf8150fb334086830133891b494
serge <sergeizahargood@gmail.com>
2020-09-11 09:04:01 +0700

    File

--- src/_Java8/_Глава_21_IO/_797_File/File1.java
+++ src/_Java8/_Глава_21_IO/_797_File/File1.java
@@ -1,9 +1,10 @@
 package _Java8._Глава_21_IO._797_File;
-public class File {
+import java.io.File;
+public class File1 {
     private void print (String s) {
         System.out.println(s);
     }
     public static void main (String[] args) {
-
+        File file = new File("C://Program Files (x86)/QTranslate.exe");
     }
 }
