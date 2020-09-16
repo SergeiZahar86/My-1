e57322c1c554c9849c6f464ac7ce92720e59b9e0
SergeiZahar86 <sergei86zahar@mail.ru>
2020-09-16 20:11:09 +0700

    1

--- src/_Java8/_Глава_21_IO/_809_FileInputStream/FileInputStreamDemo1.java
+++ src/_Java8/_Глава_21_IO/_809_FileInputStream/FileInputStreamDemo1.java
@@ -1,9 +1,19 @@
 package _Java8._Глава_21_IO._809_FileInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 public class FileInputStreamDemo1 {
     public static void main (String[] args) {
         int size;
         // Для автоматического закрытия потока ввода
         //используется оператор try с ресурсами
+        try (FileInputStream f = new FileInputStream("C://Users/Sergei/IdeaProjects/" +
+                "My-1/file1.txt")) {
 
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
