33194cba46dbff9008acaa7df49582c169018ad9
SergeiZahar86 <sergei86zahar@mail.ru>
2020-09-16 20:16:22 +0700

    1

--- src/_Java8/_Глава_21_IO/_809_FileInputStream/FileInputStreamDemo1.java
+++ src/_Java8/_Глава_21_IO/_809_FileInputStream/FileInputStreamDemo1.java
@@ -9,6 +9,32 @@ public class FileInputStreamDemo1 {
         //используется оператор try с ресурсами
         try (FileInputStream f = new FileInputStream("C://Users/Sergei/IdeaProjects/" +
                 "My-1/file1.txt")) {
+            System.out.println("Общее количество доступных байтов: " + (size =
+                    f.available()));
+            int n = size / 40;
+            System.out.println("First " + n + " байтов, прочитанных из файла по " +
+                    "очереди методом read()");
+            for (int i = 0; i < n; i++) {
+                System.out.print((char) f.read());
+            }
+            System.out.println("\nBce еще доступно: " + f.available());
+            System.out.println("Чтение следующих " + n + " байтов по очереди методом read(b[])");
+            byte[] b = new byte[n];
+            if (f.read(b) != n) {
+                System.err.println("Нельзя прочитать " + n + " байтов.");
+            }
+            System.out.println(new String(b, 0, n));
+            System.out.println("\nBce еще доступно:: " + (size = f.available()));
+            System.out.println("Пропустить половину оставшихся байтов методом skip()");
+            f.skip(size / 2);
+            System.out.println("Все еще доступно: " + f.available());
+            System.out.println("Чтение " + n / 2 + " байтов, размещаемых в конце массива");
+            if (f.read(b, n / 2, n / 2) != n / 2) {
+                System.err.println("Нельзя прочитать " + n / 2 + " bytes.");
+            }
+            System.out.println(new String(b, 0, b.length));
+            System.out.println("\nBce еще доступно:: " + f.available());
+
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
