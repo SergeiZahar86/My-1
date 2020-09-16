b6c11af88ceefa5be5eab10b9a558740fd0d42cc
SergeiZahar86 <sergei86zahar@mail.ru>
2020-09-16 20:10:04 +0700

    1

--- src/_Java8/_Глава_21_IO/_809_FileInputStream/FileInputStreamDemo1.java
+++ src/_Java8/_Глава_21_IO/_809_FileInputStream/FileInputStreamDemo1.java
@@ -1,6 +1,9 @@
 package _Java8._Глава_21_IO._809_FileInputStream;
 public class FileInputStreamDemo1 {
     public static void main (String[] args) {
+        int size;
+        // Для автоматического закрытия потока ввода
+        //используется оператор try с ресурсами
 
     }
 }
